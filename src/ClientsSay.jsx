0b60db6d8c573312b8be122f44f9b5eee19569f3
Artem468 <artem.belykh.ru@gmail.com>
2024-11-16 19:19:45 +0200

    до ламп а дальше ебать как лень

--- src/ClientsSay.jsx
+++ src/ClientsSay.jsx
@@ -1,46 +1,54 @@
-import "./ClientsSay.css"
 import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/swiper-bundle.css';
+import 'swiper/css';
 import 'swiper/css/pagination';
 import {Pagination} from 'swiper/modules';
+import "./ClientsSay.css"
+import {ClientsSayData} from "./ClientsSayData.js";
+import zapiataia from "./assets/zapiataia.svg"
+import lamps from "./assets/lamps.svg"
 
 function ClientsSay() {
     return (
-        <section className="ClientsSay">
-            <div>
-                <p>TESTIMONIAL</p>
-                <p>Our Clients Say</p>
-                <p>We love to hear from customers, so please leave a comment or say hello in an email.</p>
-            </div>
-            <Swiper
-                pagination={true}
-                modules={[Pagination]}
-                spaceBetween={50}
-                slidesPerView={1}
-                className="SwiperCard"
-            >
-                <SwiperSlide>
-                    <div>
-                        <p>qweqwe</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div>
-                        <p>qweqwe</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div>
-                        <p>qweqwe</p>
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div>
-                        <p>qweqwe</p>
-                    </div>
-                </SwiperSlide>
-            </Swiper>
-        </section>
+        <>
+            <section className="ClientsSay">
+                <div className="ClientsSayText">
+                    <p>TESTIMONIAL</p>
+                    <p>Our Clients Say</p>
+                    <p>We love to hear from customers, so please leave a comment or say hello in an email.</p>
+                </div>
+                <Swiper
+                    pagination={true}
+                    modules={[Pagination]}
+                    className="ClientsSaySwiper"
+                    slidesPerView={3}
+                    spaceBetween={50}>
+                    {
+                        ClientsSayData.map(
+                            (item, index) =>
+                                <SwiperSlide key={index} className="clientSlide">
+                                    <div className="clientComment">
+                                        <div className="ClientData">
+                                            <div className="clientPhoto">
+                                                <img src={zapiataia} alt=''/>
+                                                <img src={item.image} alt=''/>
+                                            </div>
+                                            <div className="clientInfo">
+                                                <p>{item.name}</p>
+                                                <p>{item.city}</p>
+                                            </div>
+                                        </div>
+                                        <hr/>
+                                        <div className="ClientsCommentData">
+                                            <p>{item.comment}</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                        )
+                    }
+                </Swiper>
+            </section>
+            <img className='lamps' src={lamps} alt=""/>
+        </>
     )
 }
 
---
+++ src/ClientsSayData.js
@@ -0,0 +1,54 @@
+import client1 from './assets/clients/client1.svg';
+import client2 from './assets/clients/client2.svg';
+import client3 from './assets/clients/client3.svg';
+
+export const ClientsSayData = [
+    {
+        image: client1,
+        name: "Natasha D",
+        city: "Newyork",
+        comment: "They known for its fabulous taste and food. Anywhere you go your hunger is satisfied. The best chicken & burgers those are yummy."
+    },
+    {
+        image: client2,
+        name: "Jack Sparrow",
+        city: "Salt Lake City",
+        comment: "I have visited this fantastic restaurant on several occasions, food is absolutely outstanding & attention to detail is in league of its own."
+    },
+    {
+        image: client3,
+        name: "Martin",
+        city: "San Diego",
+        comment: "Perfectly known for its fabulous taste and food. Anywhere your hunger is satisfied. They give best fried chicken and burgers those are yummy."
+    },
+    {
+        image: client1,
+        name: "Huiasha",
+        city: "Moscow",
+        comment: "They known for its fabulous taste and food. Anywhere you go your hunger is satisfied. The best chicken & burgers those are yummy."
+    },
+    {
+        image: client2,
+        name: "Biba",
+        city: "Podzalupinsk",
+        comment: "I have visited this fantastic restaurant on several occasions, food is absolutely outstanding & attention to detail is in league of its own."
+    },
+    {
+        image: client3,
+        name: "Boba",
+        city: "Anlak",
+        comment: "Perfectly known for its fabulous taste and food. Anywhere your hunger is satisfied. They give best fried chicken and burgers those are yummy."
+    },
+    {
+        image: client3,
+        name: "Martin",
+        city: "San Diego",
+        comment: "Perfectly known for its fabulous taste and food. Anywhere your hunger is satisfied. They give best fried chicken and burgers those are yummy."
+    },
+    {
+        image: client1,
+        name: "Huiasha",
+        city: "Moscow",
+        comment: "They known for its fabulous taste and food. Anywhere you go your hunger is satisfied. The best chicken & burgers those are yummy."
+    }
+]
